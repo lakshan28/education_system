12c695665a48c6bc1e7c4044ee1375628953240b
lakshan28 <lakshansampath28@gmail.com>
2020-10-13 21:17:06 +0530

    sample_front_end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
